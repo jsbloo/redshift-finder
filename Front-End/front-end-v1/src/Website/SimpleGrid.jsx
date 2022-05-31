cd23ec7ee706f4c3bc71f5041e0e922312d89bc2
Kyrone James <kyrone@Kyrones-MacBook-Pro.local>
2022-05-31 12:30:06 +0100

    some changes

---
+++ Front-End/front-end-v1/src/Website/SimpleGrid.jsx
@@ -0,0 +1,56 @@
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+import Container from 'react-bootstrap/Container'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
+import NavDropdown from 'react-bootstrap/NavDropdown'
+
+
+
+
+
+
+
+const SimpleGrid = () => {
+
+    return (
+        <>
+           
+
+            <Navbar bg="light" expand="lg">
+                <Container>
+                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="{<HomePage/>}">Home</Nav.Link>
+                            <Nav.Link href="#link">Link</Nav.Link>
+                            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+            <Container>
+                <Row>
+                    <Col>1 of 3</Col>
+                    <Col xs={10}>2 of 3 (wider)</Col>
+                    <Col>3 of 3</Col>
+                </Row>
+                <Row>
+                    <Col>1 of 3</Col>
+                    <Col xs={10}>2 of 3 (wider)</Col>
+                    <Col>3 of 3</Col>
+                </Row>
+            </Container>
+        </>
+
+    );
+}
+
+export default SimpleGrid;
