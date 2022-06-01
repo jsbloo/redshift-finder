e966a84d13854e41b8051209376ad17862620ded
Joshua-B <joshua_bloom96@yahoo.co.uk>
2022-06-01 11:32:35 +0100

    added new request

--- mern/server/routes/citizen.js
+++ mern/server/routes/citizen.js
@@ -23,9 +23,37 @@ const getCitizenById = async (req,res,next) => {
     next();
 }
 
+const getByFnSnDob = async (req,res,next) => {
+    let citizen;
+    try {
+        citizen = await CitizenSchema.find({
+            forenames: req.params.fname,
+            surname: req.params.sname,
+            dateOfBirth: req.params.dob
+        });
+
+        console.log(citizen);
+        console.log(req.params);
+
+        if(citizen == []){
+            return res.status(404).json({ message: "Cannot find citizen"});
+        }
+    }catch(e){
+        return res.status(500).json({ message: "error"});
+    }
+
+    res.citizen = citizen;
+    next();
+}
+
+
 //getById
 citizenRoutes.get('/:id', getCitizenById, (req, res) => {
     res.send(res.citizen);
 });
+
+citizenRoutes.get('/:fname/:sname/:dob', getByFnSnDob ,(req, res) => {
+    res.send(res.citizen);
+})
 
 module.exports = citizenRoutes;
