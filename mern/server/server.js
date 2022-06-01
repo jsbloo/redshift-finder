3beb41d87af8c431014118352102304a3445925e
kyrone97 <kyronej@hotmail.com>
2022-06-01 11:29:07 +0100

    updated names

--- mern/server/server.js
+++ mern/server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://admin:2gjPbKkGq6CE3ka@redshift-db.doa1vwz.mongodb.net/citizen-db?retryWrites=true&w=majority', {useNewUrlParser: true});
+mongoose.connect('mongodb+srv://admin:2gjPbKkGq6CE3ka@redshift-db.doa1vwz.mongodb.net/citizen-db?retryWrites=true&w=majority', { useNewUrlParser: true });
 const db = mongoose.connection;
 
 db.on('error', (error) => console.error(error));
@@ -13,6 +13,6 @@ app.use(express.json());
 const citizensRouter = require("./routes/citizen");
 app.use('/citizens', citizensRouter);
 
-app.listen(3000, () => console.log("Server Started"));
+app.listen(3001, () => console.log("Server Started"));
 
 
