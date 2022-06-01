0dec7ad54fa80e2edc7ef5d0af79bf6c46359682
BenningtonW <brendan.j.woods@gmail.com>
2022-06-01 15:38:50 +0100

    added profile and search components

--- mern/server/client/front-end-v1/src/Website/Search.jsx
+++ mern/server/client/front-end-v1/src/Website/Search.jsx
@@ -3,8 +3,22 @@ const Search = () => {
 
 
         <>
-            <input>  </input>
-        
+            
+
+            <form>
+                <label for="surname"> Enter Last Name: </label> 
+                <input type="text" id="surname" placeholder="Search...">  </input>
+                <button type="button" onClick={XX.XX}> Submit</button>
+                 
+            </form>
+
+            <form>
+                <label for="dob"> Date of Birth: </label>
+                <input type="date" id="dob" placeholder="Search...">  
+                </input>
+                <button type="button" onClick={XX.XX}> Submit</button>
+
+            </form>
         
         
         </>
--- mern/server/client/front-end-v1/src/Website/SearchPage.jsx
+++ mern/server/client/front-end-v1/src/Website/SearchPage.jsx
@@ -3,7 +3,29 @@ import axios from 'axios';
 import Search from './Search';
 
 const SearchPage = () => {
-    return (  );
-}
+    
+    // State
+    const [SearchList, setSearchList] = useState([]); 
+    const [loaded, setLoaded] = useState(false); 
+    // const [fetch, setFetch] = useState(true); 
+
+    // UseEffect
+    useEffect(() => {
+        setTimeout(() => {
+            // axios.get(URL we are searching for) - axios will return a promise (data to be fulfilled)
+            axios.get("XXXXX") 
+            // .then() - whatever the previous function returns, chuck it in here and do this with it
+            .then((response) => {
+                console.log(response.data); // Check what the data looks like, to see *how* to set it
+                setSearchList(response.data);
+            
+                // Once the state for beerList has been set, we can setLoaded(true)
+                setLoaded(true);
+            });
+        }, 2000)
+    },[]);
+        
+    
+};
  
 export default SearchPage;
