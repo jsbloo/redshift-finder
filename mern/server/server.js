81f7896a13b93ec64488ef551e42b279a2ea198c
Joshua-B <joshua_bloom96@yahoo.co.uk>
2022-06-01 14:36:07 +0100

    WIP adding cors, and also wip chaining person request

--- mern/server/server.js
+++ mern/server/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const { append } = require('express/lib/response');
 const app = express();
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+var cors = require('cors');
 
 mongoose.connect('mongodb+srv://admin:2gjPbKkGq6CE3ka@redshift-db.doa1vwz.mongodb.net/citizen-db?retryWrites=true&w=majority', {useNewUrlParser: true});
 const db = mongoose.connection;
@@ -9,10 +11,14 @@ db.on('error', (error) => console.error(error));
 db.once('open', () => console.log('Connected to Database'));
 
 app.use(express.json());
+app.use(cors());
 
 const citizensRouter = require("./routes/citizen");
-app.use('/citizens', citizensRouter);
+app.use('/citizens', citizensRouter.citizenRoutes);
 
-app.listen(3000, () => console.log("Server Started"));
+const personRouter = require("./routes/person");
+app.use("/persons", personRouter.personRoutes);
+
+app.listen(3001, () => console.log("Server Started"));
 
 
