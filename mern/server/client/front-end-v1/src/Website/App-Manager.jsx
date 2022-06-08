c182b46a761bbf02e88e60478b908b3d0ff7cbcd
BenningtonW <brendan.j.woods@gmail.com>
2022-06-08 15:13:16 +0100

    added about page

--- mern/server/client/front-end-v1/src/Website/App-Manager.jsx
+++ mern/server/client/front-end-v1/src/Website/App-Manager.jsx
@@ -1,13 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import About from "./About";
 import ProfilePage from './ProfilePage';
+import Features from './Features';
 
 const AppManager = () => {
     return ( 
         <BrowserRouter>
             <Routes>      
                 <Route path="/" element={<ProfilePage />} />
+                <Route path="/About" element={<About />} />
+                <Route path="/Features" element={<Features />} />
             </Routes>
         </BrowserRouter>
+        
     );
 }
 
--- mern/server/client/front-end-v1/src/Website/Navigation.jsx
+++ mern/server/client/front-end-v1/src/Website/Navigation.jsx
@@ -1,6 +1,7 @@
 import { Navbar, Container, Nav, NavDropdown } from 'react-bootstrap';
-import About from './About.jsx';
-
+import About from './About';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 
 const Navigation = () => {
@@ -9,7 +10,7 @@ const Navigation = () => {
         <>
             <Navbar bg="dark" variant="dark">
                 <Container>
-                    <Navbar.Brand href="#home">RedShift</Navbar.Brand>
+                    <Navbar.Brand href="/">RedShift</Navbar.Brand>
                     <img
                         alt="Company Logo"
                         src= {require("./CompanyLogo.PNG")}
@@ -18,12 +19,17 @@ const Navigation = () => {
                         className="d-inline-block align-top"
                     />
                     <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="./About.jsx"> About</Nav.Link>
+                        <Nav.Link href="/">Home</Nav.Link>
+                        <Nav.Link href="/Features">Features</Nav.Link>
+                        <Nav.Link href="/About"> About</Nav.Link>
                     </Nav>
                 </Container>
             </Navbar>
+            {/* <BrowserRouter>
+            <Routes>      
+                <Route path="/About" element={<About />} />
+            </Routes>
+            </BrowserRouter> */}
         </>
     )
 }
