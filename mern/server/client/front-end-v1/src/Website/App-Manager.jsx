bdc133ccb7c822aa850b77bed52ef3284a27a21b
BenningtonW <brendan.j.woods@gmail.com>
2022-06-08 15:49:43 +0100

    adding about page

--- mern/server/client/front-end-v1/src/Website/App-Manager.jsx
+++ mern/server/client/front-end-v1/src/Website/App-Manager.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ProfilePage from "./ProfilePage";
+import About from "./About"
+import Features from "./Features"
 
 const AppManager = () => {
     return (
