c31aedc1ad7fedde2751bf3de2d0b11a85be561d
DanSandvig <dspublicaddress@protonmail.com>
2022-06-13 10:28:20 +0100

    Changed ssl certs property to tls

--- mern/server/server.js
+++ mern/server/server.js
@@ -3,7 +3,7 @@ const app = express();
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-const URI = 'mongodb://ihavenoclue:noreallynotone@docdb-2022-05-31-18-46-43.cluster-cnyrbefiq91q.eu-west-2.docdb.amazonaws.com:27017/redshift?ssl=true&ssl_ca_certs=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false'
+const URI = 'mongodb://ihavenoclue:noreallynotone@docdb-2022-05-31-18-46-43.cluster-cnyrbefiq91q.eu-west-2.docdb.amazonaws.com:27017/redshift?ssl=true&tlsCAFile=rds-combined-ca-bundle.pem&replicaSet=rs0&readPreference=secondaryPreferred&retryWrites=false'
 
 mongoose.connect(URI, {
     useNewUrlParser: true
